4d434b3a5e9f4b496c74c054b9ac36748e0f0888
zazabird <zazabird@gmail.com>
2023-05-10 00:42:01 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Your AI assistant",
+  description: "Your personal AI Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Your AI assistant",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
